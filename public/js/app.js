43c30cda3e8327a5b721c1ee6a404774a477cc6f
SumitNawathe <50125892+SumitNawathe@users.noreply.github.com>
2020-07-14 17:31:40 -0400

    Spacing

--- public/js/app.js
+++ public/js/app.js
@@ -10,13 +10,13 @@ weatherForm.addEventListener('submit', (e) => {
 
     const location = search.value;
     fetch('http://localhost:3000/weather?address='+location).then((response) => {    
-    response.json().then((data) => {
-        if (data.error) {
-            messageOne.textContent = data.error;
-        } else {
-            messageOne.textContent = data.location;
-            messageTwo.textContent = data.forecastData;
-        }
-    })
-});
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error;
+            } else {
+                messageOne.textContent = data.location;
+                messageTwo.textContent = data.forecastData;
+            }
+        });
+    });
 });
